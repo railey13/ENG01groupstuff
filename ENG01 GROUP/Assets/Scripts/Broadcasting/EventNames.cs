5ee9c1a26902ecb8e124f486781c77011ca7543d
KenshinRR <krreblando15@gmail.com>
2024-05-23 15:15:23 +0800

    Added Sphere Spawner

--- ENG01 GROUP/Assets/Scripts/Broadcasting/EventNames.cs
+++ ENG01 GROUP/Assets/Scripts/Broadcasting/EventNames.cs
@@ -50,6 +50,13 @@ public class EventNames {
 		public const string ON_FIRST_SCAN = "FIRST_TARGET_SCAN";
 		public const string ON_FINAL_SCAN = "ON_FINAL_SCAN";
 	}
+
+	public class SpawnSystem
+	{
+		public const string ON_SPHERE_SPAWN = "ON_SPHERE_SPAWN";
+		public const string ON_SPHERE_DESPAWN = "ON_SPHERE_DESPAWN";
+
+    }
 }
 
 
