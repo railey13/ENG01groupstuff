c2b1acf7da80c41a36c4d2b8293a6498f33e572e
KenshinRR <krreblando15@gmail.com>
2024-06-10 16:39:59 +0800

    Implemented Customer System

--- ENG01 GROUP/Assets/Scripts/RootsFear/PersonMoodHandler.cs
+++ ENG01 GROUP/Assets/Scripts/RootsFear/PersonMoodHandler.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PersonMoodHandler : MonoBehaviour
 {
-    [SerializeField] private int patience = 3;
+    //positions to be noted
+    [SerializeField] private GameObject exitTarget;
+    [SerializeField] private GameObject EntranceReference;
+    [SerializeField] private GameObject BarReference;
 
+    //FACES
     [SerializeField] private Material HappyFace;
     [SerializeField] private Material SmileFace;
     [SerializeField] private Material MehFace;
@@ -15,7 +20,17 @@ public class PersonMoodHandler : MonoBehaviour
     [SerializeField] private GameObject Face;
 
     private Renderer faceRenderer;
+
     private bool isLeaving = false;
+    private bool isEntering = false;
+
+    private int patience = 3;
+
+    private Vector3 exitPos = Vector3.zero;
+    private Vector3 entrancePos = Vector3.zero;
+    private Vector3 barPos = Vector3.zero;
+
+    private float speed = 70;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +42,16 @@ public class PersonMoodHandler : MonoBehaviour
         //set default emotion
         this.faceRenderer = Face.GetComponent<Renderer>();
         faceRenderer.material = SmileFace;
+
+        //setting up positions
+        this.exitPos = exitTarget.transform.position;
+        this.entrancePos = EntranceReference.transform.position;
+        this.barPos = BarReference.transform.position;
+
+        //hiding
+        this.BarReference.GetComponent<Renderer>().enabled = false;
+
+        this.ChangeColor();
     }
 
     // Update is called once per frame
@@ -34,7 +59,13 @@ public class PersonMoodHandler : MonoBehaviour
     {
         if (isLeaving)
         {
+            transform.position = Vector3.MoveTowards(transform.position, exitPos, this.speed * Time.deltaTime);
+        }
 
+        if (isEntering)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, barPos, this.speed * Time.deltaTime);
+            if (transform.position == this.barPos) isEntering = false;
         }
     }
     private void OnDestroy()
@@ -59,11 +90,40 @@ public class PersonMoodHandler : MonoBehaviour
         }
 
         if (this.patience > 0)
-        this.patience--;
+            this.patience--;
+        
+        if (this.patience <= 0)
+        {
+            this.isLeaving = true;
+            this.patience = 3;
+        }
     }
 
     private void GetSatisfied()
     {
         faceRenderer.material = HappyFace;
+        this.isLeaving = true;
+        this.patience = 3;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == this.exitTarget)
+        {
+            this.isLeaving = false;
+            this.transform.position = this.entrancePos;
+            this.isEntering = true;
+            faceRenderer.material = SmileFace;
+            this.ChangeColor();
+        }
+    }
+
+    private void ChangeColor()
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f)
+                );
     }
 }
