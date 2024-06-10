31b4f0a05baa1e65d28cbca6daced74bbef4f8c3
KenshinRR <krreblando15@gmail.com>
2024-06-10 15:08:46 +0800

    Added Emotions to Person

--- ENG01 GROUP/Assets/Scripts/RootsFear/PersonMoodHandler.cs
+++ ENG01 GROUP/Assets/Scripts/RootsFear/PersonMoodHandler.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PersonMoodHandler : MonoBehaviour
 {
+    [SerializeField] private int patience = 3;
+
+    [SerializeField] private Material HappyFace;
+    [SerializeField] private Material SmileFace;
+    [SerializeField] private Material MehFace;
+    [SerializeField] private Material MadFace;
+    [SerializeField] private Material AnnoyedFace;
+
+    [SerializeField] private GameObject Face;
+
+    private Renderer faceRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //add observer
+        EventBroadcaster.Instance.AddObserver(EventNames.RootsFear.ON_ICE_FAIL, this.GetAnnoyed);
+        EventBroadcaster.Instance.AddObserver(EventNames.RootsFear.ON_ICE_SUCCESS, this.GetSatisfied);
+
+        //set default emotion
+        this.faceRenderer = Face.GetComponent<Renderer>();
+        faceRenderer.material = SmileFace;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnDestroy()
+    {
+        EventBroadcaster.Instance.RemoveObserver(EventNames.RootsFear.ON_ICE_FAIL);
+        EventBroadcaster.Instance.RemoveObserver(EventNames.RootsFear.ON_ICE_SUCCESS);
+    }
+
+    private void GetAnnoyed()
+    {
+        switch (this.patience)
+        {
+            case 3:
+                faceRenderer.material = MehFace;
+                break;
+            case 2:
+                faceRenderer.material = AnnoyedFace;
+                break;
+            case 1:
+                faceRenderer.material = MadFace;
+                break;
+        }
+        this.patience--;
+    }
+
+    private void GetSatisfied()
+    {
+        faceRenderer.material = HappyFace;
     }
 }
